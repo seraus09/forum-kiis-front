4e7e5299af50dbf14d1c84497acb672ba3411586
Serhii <sera.ua09@gmail.com>
2023-03-15 18:57:54 +0200

    added first page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import NavBar from './Components/Menu/Navbar';
+import ResetPassword from './Pages/ResetPassword'
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+  } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const App = ()=> {
+    return (    
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route exact path="/" element={<SignIn />} />
+                <Route exact path="/signin" element={<SignIn />} />
+                <Route exact path="/signup" element={<SignUp />} />
+                <Route exact path="/reset-password" element={<ResetPassword />} />
+            </Routes>
+        </Router>
+    )
 }
-
-export default App;
+export default App
